758a688879e4dde5dd09883f5e707cb3b1163840
sam-jones1 <sam.jones1@engineering.digital.dwp.gov.uk>
2022-01-31 14:29:16 +0000

    Chained plan step

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,7 @@
 variable "public_bucket_name" {
   type        = map(string)
   description = "The public bucket name for the S3 bucket"
+  default     = "sams-test-website-bucket-test"
 }
 
 variable "region" {
